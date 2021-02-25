5ff4a8d0740bd60dc206165b4e5f9b1129d54a34
sethAtCloudAhoy <seth@cloudahoy.com>
2021-02-25 10:38:21 -0500

    comments to transformer

--- transformer.py
+++ transformer.py
@@ -13,16 +13,16 @@ if __name__ == "__main__":
     from uppercase import to_uppercase
     from add_space import add_space
 
-    parser = argparse.ArgumentParser(description="Process text provided via STDIN")
+    parser = argparse.ArgumentParser(description="Transform text provided via STDIN")
     parser.add_argument('transform', nargs="?", default="")
-    parser.add_argument('--uppercase', '-u', action='store_true')
-    parser.add_argument('--addspace', '-a', action='store_true')
-    output = parser.parse_args()
+    parser.add_argument('--uppercase', '-u', action='store_true', help="Make uppercase")
+    parser.add_argument('--addspace', '-a', action='store_true', help="Add a space between each character")
+    params = parser.parse_args()
 
     transformers = []
-    if output.uppercase or output.transform == "uppercase":
+    if params.uppercase or params.transform == "uppercase":
         transformers.append(to_uppercase)
-    if output.addspace or output.transform == "addspace":
+    if params.addspace or params.transform == "addspace":
         transformers.append(add_space)
 
     if not transformers:
